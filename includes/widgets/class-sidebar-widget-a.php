cd0fb5e6eeb676a2e3c7fe78bfc8d46b6de119ce
Mike Hemberger <mike@thestizmedia.com>
2017-06-12 14:03:24 -0400

    ad widgets

---
+++ includes/widgets/class-sidebar-widget-a.php
@@ -0,0 +1,94 @@
+<?php
+
+// Register our new widget
+add_action( 'widgets_init', 'mai_aec_register_ad_widget_a' );
+function mai_aec_register_ad_widget_a() {
+    register_widget( 'Mai_AEC_Widget_A' );
+}
+
+class Mai_AEC_Widget_A extends WP_Widget {
+
+	protected static $location = 'mai_ad_widget_a';
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+	 		'mai_aec_widget_a', // Base ID
+			'Mai Ad "A"', // Name
+			array( 'description' => __( 'Display ad widget "A"', 'mai-aec' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+
+		// Get our ad code
+		$ad = maiaec_display()->get_option( $key, self::$location );
+
+		// Bail if empty
+		if ( empty( $ad ) ) {
+			return;
+		}
+
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $before_widget;
+
+		if ( ! empty( $title ) ) {
+			echo $before_title . $title . $after_title;
+		}
+
+		// Output the ad code
+		echo trim($ad);
+
+		echo $after_widget;
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = '';
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'mai-aec' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php
+	}
+
+}
