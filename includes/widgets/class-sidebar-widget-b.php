d871f6ca27345fc51810486124937b8849e93edc
Mike Hemberger <mike@thestizmedia.com>
2018-01-29 11:29:55 -0500

    fix widgets still calling class

--- includes/widgets/class-sidebar-widget-b.php
+++ includes/widgets/class-sidebar-widget-b.php
@@ -34,7 +34,7 @@ class Mai_AEC_Widget_B extends WP_Widget {
 	public function widget( $args, $instance ) {
 
 		// Get our ad code
-		$ad = maiaec_display()->get_display_global( self::$key, self::$location, false );
+		$ad = maiaec_get_display_global( self::$key, self::$location, false );
 
 		// Bail if empty
 		if ( empty( $ad ) ) {
